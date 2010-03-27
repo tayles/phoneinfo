95c41d4a83557136068c089425e1ed5b475dbdf5
tayles (Home) <git@tayles.co.uk>
2010-03-27 14:01:02 +0000

    Initial commit

---
+++ application/logs/2010-01-14.log.php
@@ -0,0 +1,13 @@
+<?php defined('SYSPATH') or die('No direct script access.'); ?>
+
+2010-01-14 02:20:19 +00:00 --- error: Missing i18n entry core.page_not_found for language en_US
+2010-01-14 02:20:19 +00:00 --- error: Missing i18n entry core.uncaught_exception for language en_US
+2010-01-14 02:20:19 +00:00 --- error: core.uncaught_exception
+2010-01-14 02:20:19 +00:00 --- error: Missing i18n entry core.error_file_line for language en_US
+2010-01-14 02:20:19 +00:00 --- error: Missing i18n entry core.error_file_line for language en_US
+2010-01-14 02:20:19 +00:00 --- error: Missing i18n entry core.error_file_line for language en_US
+2010-01-14 02:20:19 +00:00 --- error: Missing i18n entry core.error_file_line for language en_US
+2010-01-14 02:20:19 +00:00 --- error: Missing i18n entry core.error_file_line for language en_US
+2010-01-14 02:20:19 +00:00 --- error: Missing i18n entry core.error_file_line for language en_US
+2010-01-14 02:20:19 +00:00 --- error: Missing i18n entry core.stack_trace for language en_US
+2010-01-14 02:20:19 +00:00 --- error: Missing i18n entry core.stats_footer for language en_US
---
+++ application/logs/2010-01-17.log.php
@@ -0,0 +1,64 @@
+<?php defined('SYSPATH') or die('No direct script access.'); ?>
+
+2010-01-17 00:45:24 +00:00 --- error: Missing i18n entry core.uncaught_exception for language en_US
+2010-01-17 00:45:24 +00:00 --- error: core.uncaught_exception
+2010-01-17 00:45:24 +00:00 --- error: Missing i18n entry errors.4096 for language en_US
+2010-01-17 00:45:24 +00:00 --- error: Missing i18n entry core.error_file_line for language en_US
+2010-01-17 00:45:24 +00:00 --- error: Missing i18n entry core.error_file_line for language en_US
+2010-01-17 00:45:24 +00:00 --- error: Missing i18n entry core.error_file_line for language en_US
+2010-01-17 00:45:24 +00:00 --- error: Missing i18n entry core.error_file_line for language en_US
+2010-01-17 00:45:24 +00:00 --- error: Missing i18n entry core.error_file_line for language en_US
+2010-01-17 00:45:24 +00:00 --- error: Missing i18n entry core.error_file_line for language en_US
+2010-01-17 00:45:24 +00:00 --- error: Missing i18n entry core.error_file_line for language en_US
+2010-01-17 00:45:24 +00:00 --- error: Missing i18n entry core.error_file_line for language en_US
+2010-01-17 00:45:24 +00:00 --- error: Missing i18n entry core.error_file_line for language en_US
+2010-01-17 00:45:24 +00:00 --- error: Missing i18n entry core.error_file_line for language en_US
+2010-01-17 00:45:24 +00:00 --- error: Missing i18n entry core.error_file_line for language en_US
+2010-01-17 00:45:24 +00:00 --- error: Missing i18n entry core.error_file_line for language en_US
+2010-01-17 00:45:24 +00:00 --- error: Missing i18n entry core.error_file_line for language en_US
+2010-01-17 00:45:24 +00:00 --- error: Missing i18n entry core.stack_trace for language en_US
+2010-01-17 00:45:24 +00:00 --- error: Missing i18n entry core.stats_footer for language en_US
+2010-01-17 00:45:25 +00:00 --- error: Missing i18n entry core.page_not_found for language en_US
+2010-01-17 00:45:25 +00:00 --- error: Missing i18n entry core.uncaught_exception for language en_US
+2010-01-17 00:45:25 +00:00 --- error: core.uncaught_exception
+2010-01-17 00:45:25 +00:00 --- error: Missing i18n entry core.error_file_line for language en_US
+2010-01-17 00:45:25 +00:00 --- error: Missing i18n entry core.error_file_line for language en_US
+2010-01-17 00:45:25 +00:00 --- error: Missing i18n entry core.error_file_line for language en_US
+2010-01-17 00:45:25 +00:00 --- error: Missing i18n entry core.error_file_line for language en_US
+2010-01-17 00:45:25 +00:00 --- error: Missing i18n entry core.error_file_line for language en_US
+2010-01-17 00:45:25 +00:00 --- error: Missing i18n entry core.error_file_line for language en_US
+2010-01-17 00:45:25 +00:00 --- error: Missing i18n entry core.stack_trace for language en_US
+2010-01-17 00:45:25 +00:00 --- error: Missing i18n entry core.stats_footer for language en_US
+2010-01-17 00:45:27 +00:00 --- error: Missing i18n entry core.page_not_found for language en_US
+2010-01-17 00:45:27 +00:00 --- error: Missing i18n entry core.uncaught_exception for language en_US
+2010-01-17 00:45:27 +00:00 --- error: core.uncaught_exception
+2010-01-17 00:45:27 +00:00 --- error: Missing i18n entry core.error_file_line for language en_US
+2010-01-17 00:45:27 +00:00 --- error: Missing i18n entry core.error_file_line for language en_US
+2010-01-17 00:45:27 +00:00 --- error: Missing i18n entry core.error_file_line for language en_US
+2010-01-17 00:45:27 +00:00 --- error: Missing i18n entry core.error_file_line for language en_US
+2010-01-17 00:45:27 +00:00 --- error: Missing i18n entry core.error_file_line for language en_US
+2010-01-17 00:45:27 +00:00 --- error: Missing i18n entry core.error_file_line for language en_US
+2010-01-17 00:45:27 +00:00 --- error: Missing i18n entry core.stack_trace for language en_US
+2010-01-17 00:45:27 +00:00 --- error: Missing i18n entry core.stats_footer for language en_US
+2010-01-17 00:57:22 +00:00 --- error: Missing i18n entry core.page_not_found for language en_US
+2010-01-17 00:57:22 +00:00 --- error: Missing i18n entry core.uncaught_exception for language en_US
+2010-01-17 00:57:22 +00:00 --- error: core.uncaught_exception
+2010-01-17 00:57:22 +00:00 --- error: Missing i18n entry core.error_file_line for language en_US
+2010-01-17 00:57:22 +00:00 --- error: Missing i18n entry core.error_file_line for language en_US
+2010-01-17 00:57:22 +00:00 --- error: Missing i18n entry core.error_file_line for language en_US
+2010-01-17 00:57:22 +00:00 --- error: Missing i18n entry core.error_file_line for language en_US
+2010-01-17 00:57:22 +00:00 --- error: Missing i18n entry core.error_file_line for language en_US
+2010-01-17 00:57:22 +00:00 --- error: Missing i18n entry core.error_file_line for language en_US
+2010-01-17 00:57:22 +00:00 --- error: Missing i18n entry core.stack_trace for language en_US
+2010-01-17 00:57:22 +00:00 --- error: Missing i18n entry core.stats_footer for language en_US
+2010-01-17 20:29:53 +00:00 --- error: Missing i18n entry core.page_not_found for language en_US
+2010-01-17 20:29:53 +00:00 --- error: Missing i18n entry core.uncaught_exception for language en_US
+2010-01-17 20:29:53 +00:00 --- error: core.uncaught_exception
+2010-01-17 20:29:53 +00:00 --- error: Missing i18n entry core.error_file_line for language en_US
+2010-01-17 20:29:53 +00:00 --- error: Missing i18n entry core.error_file_line for language en_US
+2010-01-17 20:29:53 +00:00 --- error: Missing i18n entry core.error_file_line for language en_US
+2010-01-17 20:29:53 +00:00 --- error: Missing i18n entry core.error_file_line for language en_US
+2010-01-17 20:29:53 +00:00 --- error: Missing i18n entry core.error_file_line for language en_US
+2010-01-17 20:29:54 +00:00 --- error: Missing i18n entry core.error_file_line for language en_US
+2010-01-17 20:29:54 +00:00 --- error: Missing i18n entry core.stack_trace for language en_US
+2010-01-17 20:29:54 +00:00 --- error: Missing i18n entry core.stats_footer for language en_US
